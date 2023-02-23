49f5c0ba68bf42a3261df133d1b109143460ea39
Mangesh Suryawanshi <mangeshsu@cybage.com>
2023-02-23 20:23:18 +0530

    Reading task functionality added.

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,9 @@
 #[macro_use] extern  crate rocket;
-use std::{fs::{OpenOptions}, io::{Write}};
-use rocket::serde::{Deserialize, json::Json};
-
+use std::{fs::{OpenOptions}, io::{Write, BufRead, BufReader}};
+use rocket::{serde::{Deserialize, json::Json}};
+//use std::io::BufReader<std::fs::File>;
+//use std::io::BufRead;
+//use std::{io::{BufReader}};
 #[derive(Deserialize)]
 #[serde(crate = "rocket::serde")]
 struct Task<'r> {
@@ -22,7 +24,17 @@ fn add_task(task: Json<Task<'_>>) -> &'static str {
     "Task added successfully."
 }
 
+#[get("/readtasks")]
+fn read_tasks() -> Json<Vec<String>> {
+    let tasks = OpenOptions::new().read(true).append(true).create(true).open("task.txt")
+    .expect("unable to access task.txt");
+    let reader = BufReader::new(tasks); 
+    Json(reader.lines().map(|line|line.expect("Could not read line")).collect())
+    //let reader = BufReader::new(tasks);
+    //Json(reader.read_line().map(|line|line.expect("Could not read line")).collect())
+}
+
 #[launch]
 fn rocket() -> _ {
-    rocket::build().mount("/",routes![index, add_task])
+    rocket::build().mount("/",routes![index, add_task, read_tasks])
 }
