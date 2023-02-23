27989b4ecbe2fdd640b3f1757f30af083f23e78f
Mangesh Suryawanshi <mangeshsu@cybage.com>
2023-02-23 18:52:14 +0530

    create task functionality added.

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,28 @@
 #[macro_use] extern  crate rocket;
+use std::{fs::{OpenOptions}, io::{Write}};
+use rocket::serde::{Deserialize, json::Json};
+
+#[derive(Deserialize)]
+#[serde(crate = "rocket::serde")]
+struct Task<'r> {
+    item: &'r str
+}
 
 #[get("/")]
 fn index() -> &'static str {
     "Welcome to MS todo app using rust"
 }
 
+#[post("/addtask", data="<task>")]
+fn add_task(task: Json<Task<'_>>) -> &'static str {
+    let mut tasks = OpenOptions::new().read(true).append(true).create(true).open("task.txt").expect("task.txt not accessible.");
+    let task_item_string = format!("{}\n", task.item);
+    let task_item_bytes = task_item_string.as_bytes();
+    tasks.write(task_item_bytes).expect("Unable to write to task.txt");
+    "Task added successfully."
+}
+
 #[launch]
 fn rocket() -> _ {
-    rocket::build().mount("/",routes![index])
+    rocket::build().mount("/",routes![index, add_task])
 }
